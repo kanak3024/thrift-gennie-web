0f9feb82366d62c87c778d95208dd74e870b4ca0
kanak3024 <suryavanshikanak82@gmail.com>
2026-04-27 15:27:16 +0530

    hide nav on admin pages via ClientLayout

---
+++ app/components/ClientLayout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "./Navbar";
+import SupportChat from "./SupportChat";
+import NotificationToast from "./NotificationToast";
+import BottomNav from "./BottomNav";
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isAdmin = pathname.startsWith("/admin");
+
+  return (
+    <>
+      {!isAdmin && <Navbar />}
+      {!isAdmin && <NotificationToast />}
+      {!isAdmin && <BottomNav />}
+      <div className={!isAdmin ? "pb-20 md:pb-0" : ""}>
+        {children}
+      </div>
+      {!isAdmin && <SupportChat />}
+    </>
+  );
+}
