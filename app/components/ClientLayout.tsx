478bc1aa8c59490ec7ce634235da0e6d9a932e83
kanak3024 <suryavanshikanak82@gmail.com>
2026-04-28 19:05:15 +0530

    fix:bottome

--- app/components/ClientLayout.tsx
+++ app/components/ClientLayout.tsx
@@ -15,9 +15,9 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
       {!isAdmin && <Navbar />}
       {!isAdmin && <NotificationToast />}
       {!isAdmin && <BottomNav />}
-      <div className={!isAdmin ? "pb-20 md:pb-0" : ""}>
-        {children}
-      </div>
+      <div className={!isAdmin ? "pb-20 md:pb-0 min-h-screen" : ""}>
+  {children}
+</div>
       {!isAdmin && <SupportChat />}
     </>
   );
